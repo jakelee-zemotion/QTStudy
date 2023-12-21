a879f48f35039ffa6114c71f5d327a227d3fef96
Jake <jake.lee@z-emotion.com>
2023-12-21 11:06:06 +0900

    Write json file

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,10 @@
 #include <QtWidgets/QApplication>
 #include <QMainWindow>
 
+#include <QJsonObject>
+#include <QFile>
+#include <QJsonDocument>
+
 int main(int argc, char** argv)
 {
 	QApplication app(argc, argv);
@@ -18,6 +22,19 @@ int main(int argc, char** argv)
 	CustomMenuBar* cMenubar = new CustomMenuBar();
 
 	mainWindow.setMenuBar(cMenubar);
+
+
+	QJsonObject jsonObj;
+	jsonObj.insert("insert", 11);
+	jsonObj["test1"] = "test";
+	jsonObj["test2"] = 1;
+
+	QFile saveFile(QStringLiteral("deviceInfo.json"));
+	saveFile.open(QIODevice::WriteOnly);
+
+	QJsonDocument saveDoc(jsonObj);
+	saveFile.write(saveDoc.toJson());
+	saveFile.close();
 
 	return app.exec();
 }
